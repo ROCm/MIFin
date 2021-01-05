4beaedcd20d0f7ee006a5ea878ff0f8de2e5936b
Christopher Erb <chris.erb65@gmail.com>
2021-01-05 11:53:51 -0600

    clang-tidy for base64.cpp

--- src/base64.cpp
+++ src/base64.cpp
@@ -31,10 +31,10 @@
 
 */
 
-#include "base64.hpp"
-
 #include <algorithm>
 #include <stdexcept>
+
+#include "base64.hpp"
 
  //
  // Depending on the url parameter in base64_chars, one of
@@ -57,24 +57,29 @@ static unsigned int pos_of_char(const unsigned char chr) {
  // Return the position of chr within base64_encode()
  //
 
-    if      (chr >= 'A' && chr <= 'Z') return chr - 'A';
-    else if (chr >= 'a' && chr <= 'z') return chr - 'a' + ('Z' - 'A')               + 1;
-    else if (chr >= '0' && chr <= '9') return chr - '0' + ('Z' - 'A') + ('z' - 'a') + 2;
-    else if (chr == '+' || chr == '-') return 62; // Be liberal with input and accept both url ('-') and non-url ('+') base 64 characters (
-    else if (chr == '/' || chr == '_') return 63; // Ditto for '/' and '_'
+    char retval;
+    if      (chr >= 'A' && chr <= 'Z') { retval = chr - 'A'; }
+    else if (chr >= 'a' && chr <= 'z') { retval = chr - 'a' + ('Z' - 'A')               + 1; }
+    else if (chr >= '0' && chr <= '9') { retval = chr - '0' + ('Z' - 'A') + ('z' - 'a') + 2; }
+    else if (chr == '+' || chr == '-') { retval = 62; }// Be liberal with input and accept both url ('-') and non-url ('+') base 64 characters (
+    else if (chr == '/' || chr == '_') { retval = 63; }// Ditto for '/' and '_'
     else
- //
- // 2020-10-23: Throw std::exception rather than const char*
- //(Pablo Martin-Gomez, https://github.com/Bouska)
- //
-    throw std::runtime_error("Input is not valid base64-encoded data.");
+    {
+        //
+        // 2020-10-23: Throw std::exception rather than const char*
+        //(Pablo Martin-Gomez, https://github.com/Bouska)
+        //
+        throw std::runtime_error("Input is not valid base64-encoded data.");
+    }
+
+    return retval
 }
 
 static std::string insert_linebreaks(std::string str, size_t distance) {
  //
  // Provided by https://github.com/JomaCorpFX, adapted by me.
  //
-    if (!str.length()) {
+    if (str.length() == 0) {
         return "";
     }
 
@@ -105,7 +110,7 @@ static std::string encode_mime(String s) {
 
 template <typename String>
 static std::string encode(String s, bool url) {
-  return base64_encode(reinterpret_cast<const unsigned char*>(s.data()), s.length(), url);
+  return base64_encode(static_cast<const unsigned char*>(s.data()), s.length(), url);
 }
 
 std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len, bool url) {
@@ -123,7 +128,7 @@ std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len, b
  // the correct character set is chosen by subscripting
  // base64_chars with url.
  //
-    const char* base64_chars_ = base64_chars[url];
+    const char* base64_chars_ = gsl::at(base64_chars, url);
 
     std::string ret;
     ret.reserve(len_encoded);
@@ -166,7 +171,7 @@ static std::string decode(String encoded_string, bool remove_linebreaks) {
  // or std::string_view (requires at least C++17)
  //
 
-    if (encoded_string.empty()) return std::string();
+    if (encoded_string.empty()) { return std::string(); }
 
     if (remove_linebreaks) {
 
