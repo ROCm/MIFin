c240031bc0a844a4a990cbdc826863c5cf76d49c
Alex Bara <barai@mymail.vcu.edu>
2022-10-07 13:45:35 -0500

    merged develop and removed std::ignore in conv_fin

--- src/include/conv_fin.hpp
+++ src/include/conv_fin.hpp
@@ -1747,8 +1747,6 @@ std::vector<int> ConvFin<Tgpu, Tref>::GetInputTensorLengths()
         in_spatial_lens[0] = command["in_h"];
         // cppcheck-suppress unreadVariable
         in_spatial_lens[1] = command["in_w"];
-        std::ignore        = in_spatial_lens[0]; // ignore clang warning
-        std::ignore        = in_spatial_lens[1]; // ignore clang warning
     }
     else if(spatial_dim == 3)
     {
@@ -1758,9 +1756,6 @@ std::vector<int> ConvFin<Tgpu, Tref>::GetInputTensorLengths()
         in_spatial_lens[1] = command["in_h"];
         // cppcheck-suppress unreadVariable
         in_spatial_lens[2] = command["in_w"];
-        std::ignore        = in_spatial_lens[0]; // ignore clang warning
-        std::ignore        = in_spatial_lens[1]; // ignore clang warning
-        std::ignore        = in_spatial_lens[2]; // ignore clang warning
     }
     else
     {
