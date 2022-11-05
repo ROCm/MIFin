d58c42bbdf20369db38cc40223d9e3a84abe7c75
Chris Erb <chris.erb65@gmail.com>
2022-11-04 21:05:52 -0500

    Check params at end of perf tuning (#70)
    * add invalid params check to end of perf tuning
    
    * clang format
    
    * fix
    
    * fix

--- src/include/conv_fin.hpp
+++ src/include/conv_fin.hpp
@@ -75,6 +75,22 @@
 
 namespace fin {
 
+class ParamString
+{
+    std::string values;
+
+    public:
+    ParamString() {}
+    ParamString(std::string in_val) : values(in_val) {}
+
+    void Serialize(std::ostream& stream) const { stream << values; }
+    bool Deserialize(const std::string& s)
+    {
+        values = s;
+        return true;
+    }
+};
+
 const int INVOKE_LIMIT = 2;
 using json             = nlohmann::json;
 // TODO: Create a config class to encapsulate config
@@ -708,8 +724,19 @@ int ConvFin<Tgpu, Tref>::MIOpenPerfEval()
                 res_item["data_type"]      = ctx.problem.in_data_type;
                 res_item["direction"]      = conv_dir;
                 res_item["bias"]           = ctx.problem.bias;
-                res_item["reason"]         = "Success";
                 res_item["kernel_objects"] = kern_objs;
+
+                miopen::DbRecord record;
+                record.SetValues(solver_name, ParamString(params));
+                if(s.TestSysDbRecord(ctx, record))
+                {
+                    res_item["reason"] = "Success";
+                }
+                else
+                {
+                    res_item["reason"] = "Tuning returned invalid params";
+                    return false;
+                }
             }
             catch(const std::exception& e)
             {
@@ -1204,22 +1231,6 @@ int ConvFin<Tgpu, Tref>::TestApplicability()
     output["applicable_solvers"] = app_solvers;
     return 0;
 }
-
-class ParamString
-{
-    std::string values;
-
-    public:
-    ParamString() {}
-    ParamString(std::string in_val) : values(in_val) {}
-
-    void Serialize(std::ostream& stream) const { stream << values; }
-    bool Deserialize(const std::string& s)
-    {
-        values = s;
-        return true;
-    }
-};
 
 template <typename Tgpu, typename Tref>
 int ConvFin<Tgpu, Tref>::TestPerfDbEntries(
