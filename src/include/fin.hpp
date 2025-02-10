e4276548dc05324d3306b55e8f02b58a9e54ef1b
Christopher Erb <christopher.erb@amd.com>
2025-02-10 19:50:10 +0000

    reduce invoker benchmark runs

--- src/include/fin.hpp
+++ src/include/fin.hpp
@@ -65,8 +65,8 @@ using json = nlohmann::json;
 
 namespace fin {
 
-const int INVOKE_LIMIT   = 8;
-const int INVOKE_DISCARD = 5;
+const int INVOKE_LIMIT   = 5;
+const int INVOKE_DISCARD = 2;
 
 class BaseFin
 {
